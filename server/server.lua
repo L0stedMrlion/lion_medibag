3e79b0c5a72b40bb79acd66ab1aa66f9bd14a5e6
Mrlion <mrlion4202@gmail.com>
2024-06-08 09:57:51 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,9 +1,16 @@
+local ox_inventory = exports.ox_inventory
+
 RegisterServerEvent('lion_medibag:placeMedbag')
 AddEventHandler('lion_medibag:placeMedbag',function ()
-	exports.ox_inventory:RemoveItem(source, Config.MedibagItem, 1, nil, nil, nil)
+	ox_inventory:RemoveItem(source, Config.MedibagItem, 1, nil, nil, nil)
 end)
 
-RegisterServerEvent('lion_medibag:pickupMedbag')
-AddEventHandler('lion_medibag:pickupMedbag',function ()
-	exports.ox_inventory:AddItem(source, Config.MedibagItem, 1, nil, nil, nil)
+RegisterServerEvent("lion_medibag:canPickupMedbag")
+AddEventHandler("lion_medibag:canPickupMedbag", function(medibagNetId)
+    if ox_inventory:CanCarryItem(source, Config.MedibagItem, 1) then
+        ox_inventory:AddItem(source, Config.MedibagItem, 1, nil, nil, nil)
+        TriggerClientEvent("lion_medibag:pickupMedbagResponse", source, true, medibagNetId)
+    else
+        TriggerClientEvent("lion_medibag:pickupMedbagResponse", source, false, medibagNetId)
+    end
 end)
