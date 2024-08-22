db37d91aa3671ae7b6984c8f7f5f4a096501c6a5
Mrlion <mrlion4202@gmail.com>
2024-08-22 20:00:32 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,24 +1,25 @@
+lib.locale()
+
 local bags = {}
-local medibagprop = Config.MedibagProp
+local medibagprop, medibagtarget = Config.MedibagProp, Config.MedibagTarget
+local ox_target = exports.ox_target
 
-local function placeMedbag()
-    TriggerServerEvent('lion_medibag:placeMedbag')
-end
-RegisterNetEvent('lion_medibag:placeMedbag', placeMedbag)
+RegisterNetEvent("lion_medibag:placeMedbag")
+AddEventHandler("lion_medibag:placeMedbag", function()
+    TriggerServerEvent("lion_medibag:placeMedbag")
+end)
 
-RegisterNetEvent('lion_medibag:pickupMedbag', function()
-    local pedCoords = GetEntityCoords(cache.ped)
-    lib.playAnim(cache.ped, "random@domestic", "pickup_low", 5.0, 1.0, -1, 48, 0, 0, 0, 0)
-
-    if Config.Notifications then
-        Notify("Medibag", Config.Locale["pickedupmedbag"], 2000)
-    end
-
+RegisterNetEvent("lion_medibag:pickupMedbag")
+AddEventHandler("lion_medibag:pickupMedbag", function()
+    local playerPed = cache.ped
+    local pedCoords = GetEntityCoords(playerPed)
+    lib.playAnim(playerPed, "random@domestic", "pickup_low", 5.0, 1.0, -1, 48, 0, 0, 0, 0)
     local medibag = GetClosestObjectOfType(pedCoords, 2.0, GetHashKey(medibagprop), false, false, false)
     TriggerServerEvent("lion_medibag:canPickupMedbag", NetworkGetNetworkIdFromEntity(medibag))
 end)
 
-RegisterNetEvent("lion_medibag:pickupMedbagResponse", function(canCarry, medibagNetId)
+RegisterNetEvent("lion_medibag:pickupMedbagResponse")
+AddEventHandler("lion_medibag:pickupMedbagResponse", function(canCarry, medibagNetId)
     if canCarry then
         local medibag = NetworkGetEntityFromNetworkId(medibagNetId)
         if medibag ~= 0 then
@@ -30,35 +31,65 @@ RegisterNetEvent("lion_medibag:pickupMedbagResponse", function(canCarry, medibag
                 end
             end
         end
+    else
     end
 end)
 
-RegisterNetEvent("lion_medibag:place", function()
-    lib.requestModel(medibagprop)
-    local itemCount = lib.callback.await("ox_inventory:getItemCount", false, Config.MedibagItem, {})
-    local pedCoords = GetEntityCoords(cache.ped)
-    if itemCount >= 1 then
-        lib.playAnim(cache.ped, "random@domestic", "pickup_low", 5.0, 1.0, -1, 48, 0, 0, 0, 0)
-        placeMedbag()
-        if Config.Notifications then
-            Notify("Medibag", Config.Locale["placemedbag"], 2000)
+RegisterNetEvent("lion_medibag:place")
+AddEventHandler("lion_medibag:place", function()
+        lib.RequestModel(medibagprop)
+        local playerPed = PlayerPedId()
+        local itemCount = lib.callback.await("ox_inventory:getItemCount", false, Config.MedibagItem, {})
+        local pedCoords = GetEntityCoords(playerPed)
+        if itemCount >= 1 then
+            lib.playAnim(playerPed, "random@domestic", "pickup_low", 5.0, 1.0, -1, 48, 0, 0, 0, 0)
+            TriggerServerEvent("lion:removeitem", Config.MedibagItem)
+            local Medibag = CreateObject(medibagprop, pedCoords.x, pedCoords.y, pedCoords.z - 1, true, false, false)
+            SetEntityHeading(Medibag, GetEntityHeading(playerPed))
+            PlaceObjectOnGroundProperly(Medibag)
+            table.insert(bags, Medibag)
+            SetModelAsNoLongerNeeded(medibagprop)
+            ox_target:addLocalEntity(Medibag, medibagtarget)
         end
-        local newMedBag = CreateObject(medibagprop, pedCoords.x, pedCoords.y, pedCoords.z - 1, true, false, false)
-        SetEntityHeading(newMedBag, GetEntityHeading(cache.ped))
-        PlaceObjectOnGroundProperly(newMedBag)
-        bags[#bags + 1] = newMedBag
     end
-end)
+)
 
 AddEventHandler("onResourceStop", function(resourceName)
-    if resourceName == cache.resource then
-        for i = 1, #bags do
-            local bagId = bags[i];
-            if DoesEntityExist(bagId) then
-                DeleteEntity(bagId)
+    if resourceName == GetCurrentResourceName() then
+        for _, medibag in pairs(bags) do
+            if DoesEntityExist(medibag) then
+                DeleteEntity(medibag)
             end
         end
     end
 end)
 
-exports.ox_target:addModel(medibagprop, Config.MedibagTarget)
+if Config.Command then
+    RegisterCommand(Config.Commandname, function()
+        local admin = lib.callback.await('lion:checkperms', false)
+
+        if admin then
+            local medibagFound = false
+
+            for _, medibag in pairs(bags) do
+                if DoesEntityExist(medibag) then
+                    medibagFound = true
+                    DeleteEntity(medibag)
+                end
+            end
+
+            if medibagFound then
+                Notify("Medibag", locale("medibagsdeleted"), "fa-solid fa-suitcase-medical", "#ed1b24", 2500)
+            else
+                Notify("Medibag", locale("nomedibangsfound"), "fa-solid fa-exclamation-triangle", "#cc0000", 2500)
+            end
+
+        else
+            Notify("Server", locale("notadmin"), "fa-solid fa-user", "#cc0000", 2500)
+        end
+    end, false)
+end
+
+if Config.EnableHealing then
+    ox_target:addGlobalPlayer(Config.HealingTarget)
+end
